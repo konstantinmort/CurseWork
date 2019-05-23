7da52c1ccc2665a8608720e590d904a6ada44d13
Kevin Mort <kostydavidenko@gmail.com>
2019-05-23 23:26:06 +0700

    Комментарии в классе ChangeCustomer

--- CursSvet/ChangeCustomer.cs
+++ CursSvet/ChangeCustomer.cs
@@ -13,39 +13,39 @@ namespace CursSvet
 {
     public partial class ChangeCustomer : Form
     {
-        static String connect = "Provider=Microsoft.JET.OLEDB.4.0;data source=DB1\\Furniture1.mdb";
-        OleDbConnection con = new OleDbConnection(connect);
-        OleDbCommand command;
-        OleDbDataReader reader;
-        string query;
+        //static String connect = "Provider=Microsoft.JET.OLEDB.4.0;data source=DB1\\Furniture1.mdb";
+        //OleDbConnection con = new OleDbConnection(connect);
+        //OleDbCommand command;
+        //OleDbDataReader reader;
+        //string query;
         public ChangeCustomer()
         {
-            InitializeComponent();
-            con.Open();
+            //InitializeComponent();
+            //con.Open();
 
-            query = "SELECT ID_Customer FROM Customer";
-            command = new OleDbCommand(query, con);
-            reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                comboBox1.Items.Add(reader[0]);
-            }
+            //query = "SELECT ID_Customer FROM Customer";
+            //command = new OleDbCommand(query, con);
+            //reader = command.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    comboBox1.Items.Add(reader[0]);
+            //}
 
-            query = "SELECT NameFilm FROM Films";
-            command = new OleDbCommand(query, con);
-            reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                comboBox2.Items.Add(reader[0]);
-            }
+            //query = "SELECT NameFilm FROM Films";
+            //command = new OleDbCommand(query, con);
+            //reader = command.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    comboBox2.Items.Add(reader[0]);
+            //}
 
-            query = "SELECT HallName FROM Hall";
-            command = new OleDbCommand(query, con);
-            reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                comboBox3.Items.Add(reader[0]);
-            }
+            //query = "SELECT HallName FROM Hall";
+            //command = new OleDbCommand(query, con);
+            //reader = command.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    comboBox3.Items.Add(reader[0]);
+            //}
         }
 
         private void Button2_Click(object sender, EventArgs e)
@@ -57,33 +57,33 @@ namespace CursSvet
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            string ID, Date, Film, Price, Hall;
-            try
-            {
-                Film = comboBox2.SelectedItem.ToString();
-                query = "SELECT ID_Films FROM Films WHERE NameFilm ='" + Film + "'";
-                command = new OleDbCommand(query, con);
-                Film = command.ExecuteScalar().ToString();
+            //string ID, Date, Film, Price, Hall;
+            //try
+            //{
+            //    Film = comboBox2.SelectedItem.ToString();
+            //    query = "SELECT ID_Films FROM Films WHERE NameFilm ='" + Film + "'";
+            //    command = new OleDbCommand(query, con);
+            //    Film = command.ExecuteScalar().ToString();
 
-                Hall = comboBox3.SelectedItem.ToString();
-                query = "SELECT ID_Hall FROM Hall WHERE HallName ='" + Hall + "'";
-                command = new OleDbCommand(query, con);
-                Hall = command.ExecuteScalar().ToString();
+            //    Hall = comboBox3.SelectedItem.ToString();
+            //    query = "SELECT ID_Hall FROM Hall WHERE HallName ='" + Hall + "'";
+            //    command = new OleDbCommand(query, con);
+            //    Hall = command.ExecuteScalar().ToString();
 
-                ID = comboBox1.SelectedItem.ToString();
-                Price = textBox4.Text;
-                Date = dateTimePicker1.Value.ToString();
-                Date = Date.Replace(".", "/");
-                query = "UPDATE SessionFilm SET Films= '" + Film + "'," +
-                               "Hall = '" + Hall + "'," +
-                               "Data = #" + Date + "#," +
-                               "Price = '" + Price + "' WHERE ID_SessionFilm = " + ID;
-                command = new OleDbCommand(query, con);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception)
-            {
-            }
+            //    ID = comboBox1.SelectedItem.ToString();
+            //    Price = textBox4.Text;
+            //    Date = dateTimePicker1.Value.ToString();
+            //    Date = Date.Replace(".", "/");
+            //    query = "UPDATE SessionFilm SET Films= '" + Film + "'," +
+            //                   "Hall = '" + Hall + "'," +
+            //                   "Data = #" + Date + "#," +
+            //                   "Price = '" + Price + "' WHERE ID_SessionFilm = " + ID;
+            //    command = new OleDbCommand(query, con);
+            //    command.ExecuteNonQuery();
+            //}
+            //catch (Exception)
+            //{
+            //}
         }
     }
 }
